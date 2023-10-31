420e1dee7ebebb96b5340c41fa9966897a43e8b9
mitul200 <mitul5267@gmail.com>
2023-10-31 18:07:33 +0600

    added img in the cards

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ const diaplaylodeData =(data)=>{
         const parents =document.getElementById('parents-container')
         const creatDiv = document.createElement('div')
         creatDiv.classList.add('col')
+        const {features,image,name,published_in}=element
         creatDiv.innerHTML=`
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
+        <div class="card w-48">
+            <img src="${image ? image :"https://randomuser.me/api/portraits/thumb/men/75.jpg"}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
