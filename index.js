d02644d5f328897aaf2258857ac01f0275ce78da
mitul200 <mitul5267@gmail.com>
2023-10-31 21:43:58 +0600

    added modal lode id name

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const diaplaylodeData =(data)=>{
         const parents =document.getElementById('parents-container')
         const creatDiv = document.createElement('div')
         creatDiv.classList.add('col')
-        const {features,image,name,published_in}=element
+        const {features,image,name,published_in,id}=element
         creatDiv.innerHTML=`
         <div class="card w-48">
             <img src="${image ? image:'http://clipart-library.com/newimages/clip-art-books-15.png'}" class="card-img-top" alt="...">
@@ -22,25 +22,28 @@ const diaplaylodeData =(data)=>{
               <p class=" m-0 p-0 card-text">2.${features[1]}</p>
               <p class=" m-0 p-0 card-text">3.${features[3]}</p>
               <hr>
-              <div class="d-flex justify-itmes-center">
+              <div class="d-flex justify-content-between aling-items- baseline">
               <div>
               <h5>Google Bard</h5>
-              <div>
-              <i class="fa-regular fa-calendar-days"></i>
-              <p>${published_in}</p>
+              <div class="d-flex aling-items-center ">
+              <p><i class="fa-regular fa-calendar-days"></i></p>
+              <p class="m-0 p-0">${published_in}</p>
               </div>
-
               </div>
-              <div></div>
-              <div></div>
+              <i class="fa-solid fa-arrow-right" onclick="modalLodeByData('${id}')"></i>
               </div>
             </div>
         </div>
         `
         parents.appendChild(creatDiv)
-
-        
+  
     });
+}
 
-
+const modalLodeByData =(elementId)=>{
+    const url =`https://openapi.programming-hero.com/api/ai/tool/${elementId}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+    
 }
