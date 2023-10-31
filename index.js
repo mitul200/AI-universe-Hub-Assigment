cf800d1311d4b09654b3af0509e18e457315bfb8
mitul200 <mitul5267@gmail.com>
2023-10-31 17:30:00 +0600

    data loded and added cards

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('everyting is okey')
+const lodeData =()=>{
+    const url ='https://openapi.programming-hero.com/api/ai/tools'
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>diaplaylodeData(data.data))
+}
+
+const diaplaylodeData =(data)=>{
+    console.log(data)
+    data.tools.forEach(element => {
+        console.log(element)
+        const parents =document.getElementById('parents-container')
+        const creatDiv = document.createElement('div')
+        creatDiv.classList.add('col')
+        creatDiv.innerHTML=`
+        <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `
+        parents.appendChild(creatDiv)
+
+        
+    });
+
+
+}
